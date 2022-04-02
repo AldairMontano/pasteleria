8547b96f27e13813597228d272af8b0992e7546e
AldairMontano <aldomch123@gmail.com>
2022-04-01 18:00:32 -0600

    Primeros cambios a la pagina

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,58 @@
 <template>
   <nav>
+    <img alt="Vue logo" src="..\src\assets\logo.png" />
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-image: url("../src/assets/fondo.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  /* background-position: center; */
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  -moz-osx-font-smoothing: grayscale; */
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
   text-align: center;
   color: #2c3e50;
 }
 
 nav {
   padding: 30px;
+  display: flex;
+  justify-content: center;
+  height: 70px;
+  background-color: rgba(185, 225, 216, 0.4);
+  backdrop-filter: blur(3px);
+  align-items: center;
+  box-shadow: 0 5px 19px #f0a166;
+}
+
+nav > img {
+  margin-right: 40px;
+  width: 70px;
 }
 
 nav a {
+  text-decoration: none;
   font-weight: bold;
-  color: #2c3e50;
+  color: #f6bf96;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #f0a166;
 }
 </style>
